4293ba6e7e2528d02e7829473d15c0347a6769f3
Tuugiixtyl <tuugiiamv@gmail.com>
2023-02-28 15:07:13 +0800

    Revert "base setup"
    This reverts commit 7cde484c09b8948bde945f9c0b6cf95768b560d5.

--- maesitfab_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MaesitfabAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'maesitfab_app'
--- massitfab/urls.py
+++ massitfab/urls.py
@@ -1,17 +1,24 @@
-from django.contrib import admin
-from django.urls import path, include
+"""massitfab URL Configuration
 
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from mfApp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('api/', include('massitfab_api.urls', namespace='mfApi')),
-    path('', include('maesitfab_app.urls', namespace='mfApp')),
-
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', index, name="index"),
+    # path('cards', card_list, name="cards"),
 ]
