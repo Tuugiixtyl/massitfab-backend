bc8aaf69cfe47deb3b43e51914cfedc3f76d5032
Tuugii <85017432+Tuugiixtyl@users.noreply.github.com>
2023-04-04 00:39:16 +0800

    custom jwt auth error message to db log

--- massitfab_auth/auth_backend.py
+++ massitfab_auth/auth_backend.py
@@ -2,27 +2,34 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import BaseBackend
 from django.db import connections
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from massitfab.settings import log_error
 
 
 class Sandy(BaseBackend):   # CustomBackend
     def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()
-        with connections['outlaw'].cursor() as cursor:
-            cursor.execute("SELECT * FROM fab_user WHERE username=%s", [username])
-            row = cursor.fetchone()
-            if row:
-                user = UserModel(*row)
-                if user.check_password(password): # type: ignore
-                    return user
+        try:
+            with connections['outlaw'].cursor() as cursor:
+                cursor.execute("SELECT * FROM fab_user WHERE username=%s", [username])
+                row = cursor.fetchone()
+                if row:
+                    user = UserModel(*row)
+                    if user.check_password(password): # type: ignore
+                        return user
+        except Exception as e:
+            log_error('create_product', '{}', str(e))
         return None
 
     def get_user(self, user_id):
         UserModel = get_user_model()
-        with connections['outlaw'].cursor() as cursor:
-            cursor.execute("SELECT * FROM fab_user WHERE id=%s", [user_id])
-            row = cursor.fetchone()
-            if row:
-                return UserModel(*row)
+        try:
+            with connections['outlaw'].cursor() as cursor:
+                cursor.execute("SELECT * FROM fab_user WHERE id=%s", [user_id])
+                row = cursor.fetchone()
+                if row:
+                    return UserModel(*row)
+        except Exception as e:
+            log_error('create_product', '{}', str(e))
         return None
 
 
